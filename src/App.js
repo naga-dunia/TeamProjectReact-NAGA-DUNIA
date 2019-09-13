bcd515ae87f87e3436a71771c521856ba9240447
anbya <anbyaalibia@gmail.com>
2019-09-13 17:12:08 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
-<<<<<<< HEAD
 // import logo from './logo.svg';
 import './App.css';
 import SignIn from "./components/signIn"
 import { BrowserRouter as Router, Route } from "react-router-dom";
-function App() {
-  return (
-    <Router>
-   
-    <Route path='/' exact component={SignIn} />
-    
-    
-    </Router>
-    
-=======
-import './App.css';
 import Dashboard from './components/dashboard.js'
-
+// function App() {
+//   return (
+//     <Router>
+   
+//     <Route path='/' exact component={SignIn} />
+    
+    
+//     </Router>
 
 function App() {
   return (
     <div>
     <Dashboard />
     </div>
->>>>>>> origin/arsya
   );
 }
 
