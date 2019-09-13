beff74c175c8f32b5896d32eb2bd45d041efcbea
anbya <anbyaalibia@gmail.com>
2019-09-13 17:13:06 +0700

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import SignIn from "./components/signIn"
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import SignIn from "./components/signIn"
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from './components/dashboard.js'
 // function App() {
 //   return (
